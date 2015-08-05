d6091415827e01b3bc6b9ca18fdfacf2ce0d231f
Casey Bodley <cbodley@redhat.com>
2015-08-05 16:20:38 -0400

    memstore: use thread_local page vector
    avoid allocating page_vector storage for every read/write request. this
    improves the sequential write bandwidth in osbench by 5-10%
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/os/MemStore.cc
+++ src/os/MemStore.cc
@@ -327,7 +327,6 @@ int MemStore::read(
   return o->read(offset, l, bl);
 }
 
-
 int MemStore::fiemap(coll_t cid, const ghobject_t& oid,
 		     uint64_t offset, size_t len, bufferlist& bl)
 {
@@ -1426,22 +1425,26 @@ int MemStore::BufferlistObject::truncate(uint64_t size)
 }
 
 // PageSetObject
+
+// use a thread-local vector for the pages returned by PageSet, so we
+// can avoid allocations in read/write()
+thread_local PageSet::page_vector MemStore::PageSetObject::tls_pages;
+
 int MemStore::PageSetObject::read(uint64_t offset, uint64_t len, bufferlist& bl)
 {
   const auto start = offset;
   const auto end = offset + len;
   auto remaining = len;
 
-  PageSet::page_vector pages;
-  data.get_range(offset, len, pages);
+  data.get_range(offset, len, tls_pages);
 
   // allocate a buffer for the data
   buffer::ptr buf(len);
 
-  auto p = pages.begin();
+  auto p = tls_pages.begin();
   while (remaining) {
     // no more pages in range
-    if (p == pages.end() || (*p)->offset >= end) {
+    if (p == tls_pages.end() || (*p)->offset >= end) {
       buf.zero(offset - start, remaining);
       break;
     }
@@ -1469,6 +1472,8 @@ int MemStore::PageSetObject::read(uint64_t offset, uint64_t len, bufferlist& bl)
     ++p;
   }
 
+  tls_pages.clear(); // drop page refs
+
   bl.append(buf);
   return len;
 }
@@ -1478,10 +1483,9 @@ int MemStore::PageSetObject::write(uint64_t offset, const bufferlist &src)
   unsigned len = src.length();
 
   // make sure the page range is allocated
-  PageSet::page_vector pages;
-  data.alloc_range(offset, src.length(), pages);
+  data.alloc_range(offset, src.length(), tls_pages);
 
-  auto page = pages.begin();
+  auto page = tls_pages.begin();
 
   // XXX: cast away the const because bufferlist doesn't have a const_iterator
   auto p = const_cast<bufferlist&>(src).begin();
@@ -1497,6 +1501,7 @@ int MemStore::PageSetObject::write(uint64_t offset, const bufferlist &src)
   }
   if (data_len < offset)
     data_len = offset;
+  tls_pages.clear(); // drop page refs
   return 0;
 }
 
@@ -1517,13 +1522,13 @@ int MemStore::PageSetObject::truncate(uint64_t size)
     return 0;
 
   // write zeroes to the rest of the last page
-  PageSet::page_vector pages;
-  data.get_range(page_offset, page_size, pages);
-  if (pages.empty())
+  data.get_range(page_offset, page_size, tls_pages);
+  if (tls_pages.empty())
     return 0;
 
-  auto page = pages.begin();
+  auto page = tls_pages.begin();
   auto data = (*page)->data;
   std::fill(data + (size - page_offset), data + page_size, 0);
+  tls_pages.clear(); // drop page ref
   return 0;
 }
