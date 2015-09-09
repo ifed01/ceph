b67c947900c0bb4a576b6266a8beda4a4376acda
Alyona Kiseleva <akiselyova@mirantis.com>
2015-09-09 17:24:14 +0300

    compression zlib modified

--- src/erasure-code/zlib/CompressionZlib.cc
+++ src/erasure-code/zlib/CompressionZlib.cc
@@ -34,15 +34,74 @@ _prefix(std::ostream* _dout)
 int CompressionZlib::encode(const bufferlist &in,
                             bufferlist *encoded)
 {
-  *encoded = in;
-  encoded->append("123");
-  _profile["plugin_name"] = "CompressionFake";
-  return 0;
+	char* data = in.c_str();
+    int ret, flush;
+    unsigned have;
+    z_stream strm;
+    int CHUNK = sizeof(data);
+    unsigned char *in = new unsigned char [CHUNK];
+    unsigned char *out = new unsigned char [CHUNK];
+
+    /* allocate deflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit(&strm, level);
+    if (ret != Z_OK)
+        return ret;
+
+    strm.avail_in = data;
+    flush = Z_FINISH;
+    strm.next_in = in;
+
+    do {
+        strm.avail_out = CHUNK;
+        strm.next_out = out;
+        ret = deflate(&strm, flush);    /* no bad return value */
+        assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+        encoded->append(ret);
+        have = CHUNK - strm.avail_out;
+    } while (strm.avail_out == 0);
+    assert(strm.avail_in == 0);     /* all input will be used */
+
+    assert(ret == Z_STREAM_END);        /* stream will be complete */
+
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return 0;
 }
 
 int CompressionZlib::decode(const bufferlist &in,
                             bufferlist *decoded)
 {
-  *decoded = in;
-  return 0;
+	char* data = in.c_str();
+	int ret, flush;
+	unsigned have;
+	z_stream strm;
+	int CHUNK = sizeof(data);
+	unsigned char *in = new unsigned char [CHUNK];
+	unsigned char *out = new unsigned char [CHUNK];
+	strm.avail_in = data;
+    strm.next_in = in;
+
+    do {
+        strm.avail_out = CHUNK;
+        strm.next_out = out;
+        ret = inflate(&strm, Z_NO_FLUSH);
+        assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+        switch (ret) {
+        case Z_NEED_DICT:
+            ret = Z_DATA_ERROR;     /* and fall through */
+        case Z_DATA_ERROR:
+        case Z_MEM_ERROR:
+            (void)inflateEnd(&strm);
+            return ret;
+        }
+        have = CHUNK - strm.avail_out;
+        decoded->append(ret);
+    } while (strm.avail_out == 0);
+
+	(void)inflateEnd(&strm);
+	return ret == Z_STREAM_END ? 0 : Z_DATA_ERROR;
+ //return 0;
 }
