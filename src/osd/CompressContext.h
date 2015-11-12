f5590c1eeeb05ac1c04d83cd5d414023e1baff69
Igor Fedotov <ifedotov@mirantis.com>
2015-11-12 19:38:34 +0300

    adding Copiright headers

--- src/osd/CompressContext.h
+++ src/osd/CompressContext.h
@@ -1,5 +1,18 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 Mirantis, Inc.
+ *
+ * Author: Igor Fedotov <ifedotov@mirantis.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
 
 #ifndef COMPRESSCONTEXT_H
 #define COMPRESSCONTEXT_H
