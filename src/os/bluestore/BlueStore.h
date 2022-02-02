b87b928925e92bdc92126ef20c096aa0e0880848
Igor Fedotov <ifedotov@suse.com>
2022-02-02 18:35:50 +0300

    refactr WAL stuff ahead of txcs merge support

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1769,7 +1769,6 @@ public:
     uint64_t osd_pool_id = META_POOL_ID;    ///< osd pool id we're operating on
 
     IOContext ioc;
-    IOContext ioc2;
     bool had_ios = false;  ///< true if we submitted IOs before our kv txn
 
     uint64_t seq = 0;
@@ -1797,7 +1796,6 @@ public:
       : ch(c),
 	osr(o),
 	ioc(cct, this),
-	ioc2(cct, this),
 	start(ceph::mono_clock::now()) {
       last_stamp = start;
       if (on_commits) {
@@ -2710,13 +2708,6 @@ private:
 public:
   void txc_aio_finish(TransContext *txc, bool last) {
     txc->last_aio_finish = last;
-    _txc_state_proc(txc);
-  }
-  void txc_wal_finish(uint64_t seq, void* p) {
-    TransContext* txc = static_cast<TransContext*>(p);
-    txc->wal_seq = seq;
-    //OpSequencer *osr = txc->osr.get();
-    //std::lock_guard l(osr->qlock);
     _txc_state_proc(txc);
   }
 private:
